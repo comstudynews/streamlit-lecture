c30cc2fdd3cb18d4aaaed97e706bc84aa6ffec29
BEOMJOON KIM <96456853+comstudynews@users.noreply.github.com>
2026-09-24 23:40:16 +0900

    feat: add step08_final_streamlit_rag/practice/chat.py

---
+++ step08_final_streamlit_rag/practice/chat.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# TODO
+# 1. message_list와 session_id를 st.session_state에 저장하세요.
+# 2. 이전 메시지를 다시 출력하세요.
+# 3. get_ai_response()의 stream을 st.write_stream()에 연결하세요.
+# 4. AI 답변을 message_list에 저장하세요.
